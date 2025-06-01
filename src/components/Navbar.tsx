2c8c826d996a16e564425c3a93f077d3a3445e45
HackerOrWottt <abhishekvarma123j@gmail.com>
2025-06-01 11:57:48 +0530

    Create a Navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from 'react'
+import Image from 'next/image';
+import { Button } from './ui/button';
+
+function Navbar() {
+  return (
+    <nav className='bg-white h-20 py-2 px-3 flex justify-between items-center'>
+        <Image className='rounded-4xl' src="/logo.jpeg" width={70} height={70} alt="Logo"></Image>
+        <h1 className='text-white'>Home</h1>
+        <h1 className='text-blue-400'>Issues</h1>
+        <h1 className='text-blue-400'>Events</h1>
+        <h1 className='text-blue-400'>Feedback</h1>
+        <h1 className='text-blue-400'>Contact</h1>
+        <h1 className='text-blue-400'>About Us</h1>
+
+        <Button className='bg-white text-blue-600' variant="outline">Sign In</Button>
+        <Button className='bg-blue-600 hover:bg-blue-800 text-white'>Register</Button>
+    </nav>
+  )
+}
+
+export default Navbar
